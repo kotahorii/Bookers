cd8d99c6322f60ee3de6a7623f25d7f1f17f35c4
kotahorii <khorii.micron@gmail.com>
2021-10-04 15:38:13 +0000

    [Update] validate, success messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,13 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -21,15 +25,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params
+    @book = Book.find(params[:id])
+    if @book.update(book_params
     )
-    redirect_to book_path(book.id)
+    flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:success] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
